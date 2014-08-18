f34a56c711b9ff43040a3484aa94f98fe5d0c14e
James <jameswmorris@gmail.com>
2014-08-18 15:24:01 +0900

    Added caching, fixed issues due to DOM changes

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Alfred v2 workflow for searching beta.jisho.org.
 
 ![Alt text](https://dl.dropboxusercontent.com/u/3781820/alfred-jisho-workflow.png?raw=true)
 
-The keyword is `jisho` and can be used as follows.
+The keyword is `jishos` and can be used as follows.
 
-`jisho {query}`
+`jishos {query}`
 
 Clicking an entry will copy it to your clipboard. If you shift-click an entry it will open the words page on beta.jisho.org.
 
@@ -25,12 +25,14 @@ The workflow is also compatible with the following beta.jisho.org search modifie
 
 These can be added to the end of your search query. For example, to return words that are common nouns you can do the following.
 
-`jisho {query} #common #noun`
+`jishos {query} #common #noun`
 
+Query results are now cached to improve performance. To remove the cached queries you can use the following command.
+
+`jisho clear`
 
 ###[DOWNLOAD](https://github.com/Jeemusu/alfred-jisho-workflow/blob/master/jisho.org.alfredworkflow?raw=true)
 
 
 ##TODO
-- Performance improvements.
 - Add compatibility for sentance results.
--- jisho.php
+++ jisho.php
@@ -1,125 +1,136 @@
 <?php
 
-class Jisho 
-{
-	public function getResults($query)
-	{
+class Jisho {
+	
+	public function __construct() {}
 
+
+	public function getResults($query) 
+	{
 		$wf = new Workflows("jwm.alfed.jisho");
 
-		// limit requests to > 2 characters
-		// * this kills alfreds placeholder, but improves performance
+		$query = trim($query);
+		
+		// Limit requests to > 2 characters
 		if(strlen($query) < 3) {
 			echo $wf->toxml();
-			exit;
 		}
 
-		$url = "http://beta.jisho.org/search/".rawurlencode(trim($query));
+		// Check cache first
+		if($this->isCached($query)) 
+		{
+			$html_data = $this->getCachedResults($query);
+		} 
+		else 
+		{
+			// Scrape HTML from jisho.org
+			$html_data = $wf->request("http://beta.jisho.org/search/".rawurlencode($query));
 
-		// make a CURL request using the workflows utility class
-		$html_data = $wf->request($url);
+			// Cache results
+			$this->cacheResults(trim($query), $html_data);
+		}
 		
-		// scrape the resulting HTML
-		$search_results = $this->scrapeHTML($html_data);
+		// Parse HTML
+		$search_results = $this->scrape($html_data);
 		
+		// Feed results to Alfred
 		if (!empty($search_results)) 
 		{
 			foreach($search_results as $result) 
 			{
+				$definition = '';
+				
+				foreach($result['en'] as $word) {
+					$definition.= $word . '・';
+				}
+				
+				$definition = preg_replace('/[^,.\s;a-zA-Z0-9_-]|[,.;]$/s', '', $definition);
+				$type = $result['type'] ? '[' . $result['type'] . '] ' : '';
+				$definition = $type . str_replace('.','; ', $definition);
+
 				$icon = $this->getIcon($result['type']);
-
-				// params: uid, arg, title, subtitle, icon, valid, autocomplete[optional]
-				$wf->result(time(), $result['url'].','.$result['ja'], $result['ja'], $result['meaning'], 'icons/'.$icon, "yes");
+				
+				$wf->result(time(), $result['url'].','.$result['ja'], $result['ja'], $definition, $icon, "yes");
 			}
-		}
-
+		}	
+		
+		// Return results to Alfred as XML
 		echo $wf->toxml();
 	}
 
-	public function scrapeHTML($input) 
+	// Scrape result HTML for data
+	public function scrape($input) 
 	{
-		// suppress warnings or xml isn't accepted by alfred
+	
+		// Suppress warnings relating to XML markup
 		libxml_use_internal_errors( true );
 		libxml_clear_errors();
 		
-		$html = new DOMDocument();
-		$html->loadHtml($input);
-		$xpath = new DOMXPath($html);
+		$doc = new DOMDocument();
+		$doc->loadHtml($input);
+		$xpath = new DOMXPath($doc);
 
-		// results array for alfred
-		$alfred_results = array();
+		$nodes = $xpath->query("//div[contains(concat(' ',@class,' '),' concept_light ')]");
 
-		$results = $xpath->query("//div[@id='primary']/div/div[@class='concept_light']");
+		foreach($nodes as $i=>$result) 
+		{
 
-		foreach ($results as $i=>$result) {
+
+			// Get Japanese Word
+			$readings = $xpath->query("div/div/div[contains(concat(' ',@class,' '),' concept_light-representation ')]/span[contains(concat(' ',@class,' '),' text ')]", $result);
 			
-			// scrape the Japanese Word
-			$readings = $xpath->query("div/div[@class='concept_light-representation']", $result);
-
-			foreach($readings as $j=>$reading) {
-
-				$words = $xpath->query("span[@class='text']", $reading);
-				
-				$temp = '';
-				foreach($words as $word){
-					$temp.=trim($word->nodeValue);
-					if($j>0) {
-						$temp.=' ・ '.trim($word->nodeValue);
-					}
-					
-				}
-				$alfred_results[$i]['ja'] = $temp;
+			foreach($readings as $word) {
+				$alfred_results[$i]['ja'] = trim($word->nodeValue);
 			}
 
-			// scrape the meaning
-			$meanings = $xpath->query("div[@class='concept_light-meanings']", $result);
 
-			foreach($meanings as $j=>$meaning) {
+			// Get result details
+			$definitions = $xpath->query("div[contains(concat(' ',@class,' '),' concept_light-meanings ')]/div[contains(concat(' ',@class,' '),' meanings-wrapper ')]", $result);
 
-				// scrape the type of word (verb, noun, etc)
-				$types = $xpath->query("p/span[@class='meaning-wrapper']/span[@class='meaning-tags']", $meaning);
-	
+			foreach($definitions as $j=>$definition) 
+			{
+				// Get type (verb, noun, etc)
+				$types = $xpath->query("div[contains(concat(' ',@class,' '),' meaning-tags ')]", $definition);
+				
+				$type_arr = array();
+				
 				foreach($types as $type){
-
-					$type = $this->getType(trim(strtolower($type->nodeValue)));
-					$alfred_results[$i]['type'] = $type;
-
-					if($type) {
-						$type = "[$type] ";
-					}
-
-					// only get the first one, or things gets complex
+					$alfred_results[$i]['type'] = $this->getType(trim(strtolower($type->nodeValue)));
 					break;
 				}
+				
 
-				// scrape the meaning
-				$words = $xpath->query("p/span[@class='meaning-wrapper']/span[@class='meaning-meaning']", $meaning);
-	
-				foreach($words as $word){
-					$alfred_results[$i]['meaning'] = $type . trim($word->nodeValue);
+				// Get definitions
+				$words = $xpath->query("div[contains(concat(' ',@class,' '),' meaning-wrapper ')]/div[contains(concat(' ',@class,' '),' meaning-definition ')]/span[contains(concat(' ',@class,' '),' meaning-meaning ')]", $definition);
+		
+				foreach($words as $z=>$word){
+					$alfred_results[$i]['en'][] = trim($word->nodeValue);
 				}
 			}
+			
+			
+			// Get Link
+			$anchors = $xpath->query("a[contains(concat(' ',@class,' '),' light-details_link ')]", $result);
 
-			// scrape the 'read more' url link
-			$anchors = $xpath->query("div/a[@class='light-details_link']", $result);
-
-			foreach($anchors as $j=>$anchor) {
+			foreach($anchors as $anchor) {
 				$alfred_results[$i]['url'] = $anchor->getAttribute("href");
 			}
-
 		}
-		
+
 		return $alfred_results;
 	}
 
-	public function getType($type) {
 
-		if(strpos($type,'i-adjective') !== false) {
+    // Returns a shortened form of the word type
+	public function getType($type) 
+	{
+
+		if(strpos($type, 'noun') !== false) {
+			$res = 'noun';
+		} else if(strpos($type,'i-adjective') !== false) {
 			$res = 'i-adj';
 		} else if(strpos($type,'na-adjective') !== false) {
 			$res = 'na-adj';
-		} else if(strpos($type,'noun') !== false) {
-			$res = 'noun';
 		} else if(strpos($type,'adverb') !== false) {
 			$res = 'adverb';
 		} else if(strpos($type,'no-adjective') !== false) {
@@ -128,29 +139,71 @@ class Jisho
 			$res = 'transitive verb';
 		} else if(strpos($type,'intransitive verb') !== false) {
 			$res = 'intransitive verb';
-		} else if(strpos($type,'intransitive verb') !== false) {
-			$res = 'intransitive verb';
 		} else if(strpos($type,'ichidan verb') !== false) {
 			$res = 'ichidan verb';
+		} else {
+			$res = '';
 		}
 
 		return $res;
 	}
 
-	public function getIcon($type) {
+    
+    // Returns an icon for the word type
+	public function getIcon($type) 
+	{
 
-		if($type == 'i-adj' || $type == 'na-adj' || $type == 'no-adj') {
+		if($type == 'i-adj' || $type == 'na-adj') {
+
 			$icon = 'icon_adj.png';
-		} else if($type =='noun') {
+
+		} else if($type == 'noun') {
+
 			$icon = 'icon_noun.png';
-		} else if(strpos($type,'adverb') !== false) {
+
+		} else if ($type == 'no-adj') {
+			
+			$icon = 'icon_adj.png';
+			
+		} else if($type == 'adverb') {
+
 			$icon = 'icon_adverb.png';
+
 		} else if(strpos($type,'verb') !== false) {
+
 			$icon = 'icon_verb.png';
+
 		} else {
 			$icon = '';
 		}
 
 		return $icon;
 	}
+
+
+	public function getCachedResults($query) 
+	{
+		return file_get_contents(getcwd().'/cache/' . $query . '.html');
+	}
+	
+	public function cacheResults($query, $results) 
+	{
+		file_put_contents(getcwd().'/cache/' . $query . '.html', $results);
+	}
+	
+	public function isCached($query) 
+	{
+		return file_exists(getcwd().'/cache/' . $query . '.html');
+	}
+	
+	// Clear cached search queries
+	public function clearCache() 
+	{
+		$files = glob(getcwd().'/cache/*');
+		foreach($files as $file){
+		  	if(is_file($file))
+		    	unlink($file); 
+		}
+	}	
+	
 }
